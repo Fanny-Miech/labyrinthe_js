146ed823762181f0a450411d69242a420ddc02f8
fanny <fanny.miech@le-campus-numerique.fr>
2020-11-18 09:52:40 +0100

    display maze

--- assets/app.js
+++ assets/app.js
@@ -6,10 +6,13 @@ $.getJSON("./data/labyrinthes.json", function (data) {
   console.log("An error has occurred.");
 });
 
-
 $(document).ready(function () {
   const getMaze = (param1, param2) => {
     return allData[param1][param2];
+  };
+
+  const clear = (element) => {
+    $(`${element}`).remove();
   };
 
   //   getMaze("4", "ex-0");
@@ -24,14 +27,13 @@ $(document).ready(function () {
   };
 
   $("#mazeChoice").on("change", function () {
-    $("#mazeSelected option").remove();
+    clear("#mazeSelected option");
     let current_size = $("#mazeChoice").val();
 
     for (const name in allData[current_size]) {
       $("#mazeSelected").append(`<option value="${name}"> ${name}</option>`);
     }
   });
-
 
   $("#isReady").click(function () {
     isReady();
@@ -43,10 +45,47 @@ $(document).ready(function () {
   let mazeSelected;
 
   $("#mazeSelected").on("change", function () {
+    clear("#grid-container div");
     let size = $("#mazeChoice").val();
     let name = $("#mazeSelected").val();
     mazeSelected = getMaze(`${size}`, `${name}`);
     console.log(mazeSelected);
+    displayMaze(mazeSelected, size);
   });
-});
 
+  function displayMaze(maze, size) {
+
+    let box = 100;
+    if (size>=20) box = 20;
+    else if(size>=15) box = 30;
+    else if(size>=10) box = 50;
+
+    document.getElementById(
+      "grid-container"
+    ).style.gridTemplateColumns = `repeat(${size}, ${box}px)`;
+    document.getElementById(
+      "grid-container"
+    ).style.gridTemplateRows = `repeat(${size}, ${box}px)`;
+
+    for (let i = 0; i < maze.length; i++) {
+      let borderstyle = "";
+
+      for (let j = 0; j < maze[i]["walls"].length; j++) {
+        if (maze[i]["walls"][j]) {
+          borderstyle = borderstyle + "solid ";
+        } else {
+          borderstyle = borderstyle + "none ";
+        }
+      }
+
+      console.log(borderstyle, "cell n° " + i);
+      let element = document.createElement("DIV");
+      element.className = "cell cell_" + i;
+      if (i == maze.length - 1) {
+        element.className = "cell cell_end";
+      }
+      element.style.borderStyle = borderstyle;
+      document.getElementById("grid-container").appendChild(element);
+    }
+  }
+});
